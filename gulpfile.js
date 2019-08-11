3e360dee5120416af1df0615de3597023c0beacf
BoltachevAndrei <andy_bolt@mail.ru>
2019-08-12 01:33:37 +0400

    Улучшил скорость загрузки

--- gulpfile.js
+++ gulpfile.js
@@ -15,6 +15,9 @@ var svgstore = require("gulp-svgstore");
 var posthtml = require("gulp-posthtml");
 var include = require("posthtml-include");
 var del = require("del");
+var htmlmin = require("gulp-htmlmin");
+var uglify = require('gulp-uglify');
+var pipeline = require('readable-stream').pipeline;
 
 gulp.task("css", function () {
   return gulp.src("source/less/style.less")
@@ -68,7 +71,6 @@ gulp.task("copy", function() {
   return gulp.src([
     "source/fonts/**/*.{woff,woff2}",
     "source/img/**",
-    "source/js/**",
     "source/*.ico"
   ], {
     base: "source"
@@ -80,12 +82,31 @@ gulp.task("clean", function() {
   return del("build");
 });
 
+gulp.task("htmlminify", function() {
+  return gulp.src('build/*.html')
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(gulp.dest('build'));
+});
+
+gulp.task('jsminify', function () {
+  return pipeline(
+    gulp.src('source/js/*.js'),
+    uglify(),
+    rename ({
+      suffix: ".min"
+    }),
+    gulp.dest('build/js')
+  );
+});
+
 gulp.task("build", gulp.series(
   "clean",
   "copy",
   "css",
   "sprite",
-  "html"
+  "html",
+  "htmlminify",
+  "jsminify"
 ));
 
 gulp.task("server", function () {
